a3043ec57ab7be6f8e802824d483b06070a39118
Louis Navarre <navarre.louis@student.uclouvain.be>
2021-03-30 19:32:20 +0200

    Remove printf

--- src/simple_lwt_seg6local.c
+++ src/simple_lwt_seg6local.c
@@ -191,7 +191,7 @@ static int send_repairSymbol_XOR(void *ctx, void *data, size_t data_sz) {
      * ->tlv: the TLV to be added in the SRH header 
      */
     const struct repairSymbol_t *repairSymbol = (struct repairSymbol_t *)data;
-    printf("CALL TRIGGERED!\n");
+    //printf("CALL TRIGGERED!\n");
 
     ++total;
     return 0;
