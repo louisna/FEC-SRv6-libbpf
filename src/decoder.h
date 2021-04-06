88524b6ff00821cf80b73e348c765489dcc68211
Louis Navarre <navarre.louis@student.uclouvain.be>
2021-04-06 19:46:30 +0200

    Add files RLC + comment on previous commit
    Add files for "clean" RLC based on the github of Francois Michel
    (UCLouvain). Also implemented the sender RLC in the user space because
    too much iterations for the kernel space + I think it will remove some
    overhead from the kernel space.
    
    Tested in the sense that the repair symbols have the correct TLV, but I
    do not know yet if the coded payload is correct, must see with the
    decoder

---
+++ src/decoder.h
@@ -0,0 +1,46 @@
+#include "fec_srv6.h"
+
+#define BPF_ERROR BPF_DROP  // Choose action when an error occurs in the process
+
+#define MAX_BLOCK 5  // Number of blocks we can simultaneously store
+#define MAX_SOURCE_SYMBOLS 25  // Number of source symbols per block
+
+#define RLC_RECEIVER_BUFFER_SIZE 16
+#define RLC_WINDOW_SIZE 4  // Usefull ?
+#define RLC_WINDOW_SLIDE 2 // Usefull ?
+
+/* Structures */
+struct sourceSymbol_t {
+    __u8 packet[MAX_PACKET_SIZE];
+    __u16 packet_length;
+    struct tlvSource__block_t tlv;
+} BPF_PACKET_HEADER;
+
+struct repairSymbol_t {
+    __u8 packet[MAX_PACKET_SIZE];
+    __u16 packet_length; // TODO: change to unsigned short ?
+    struct tlvRepair__block_t tlv;
+};
+
+struct sourceBlock_t {
+    __u16 blockID;
+    __u8 receivedSource;
+    __u8 receivedRepair;
+    __u8 nss;
+    __u8 nrs;
+};
+
+typedef struct {
+    struct repairSymbol_t repairSymbol;
+    __u8 received_ss;
+    __u8 received_rs;
+    __u32 encodingSymbolID;
+} window_info_t;
+
+typedef struct fecConvolution {
+    __u32 encodingSymbolID;
+    __u16 repairKey;
+    __u8 ringBuffSize; // Number of packets for next coding in the ring buffer
+    struct sourceSymbol_t sourceRingBuffer[RLC_RECEIVER_BUFFER_SIZE];
+    window_info_t windowInfoBuffer[RLC_RECEIVER_BUFFER_SIZE];
+} fecConvolution_t;
---
+++ src/encoder.h
@@ -0,0 +1,40 @@
+#include "fec_srv6.h"
+
+#define BPF_ERROR BPF_DROP
+
+#define RLC_BUFFER_SIZE 4
+#define RLC_WINDOW_SIZE 4
+#define RLC_WINDOW_SLIDE 2
+
+/* Structures */
+struct sourceSymbol_t {
+    __u16 packet_length;
+    __u8 packet[MAX_PACKET_SIZE];
+};
+
+struct repairSymbol_t {
+    int packet_length;
+    __u8 tlv[sizeof(struct tlvRepair__block_t)];
+    __u8 packet[MAX_PACKET_SIZE];
+};
+
+typedef struct mapStruct {
+    __u16 soubleBlock;
+    __u16 sourceSymbolCount;
+    struct sourceSymbol_t sourceSymbol;
+    struct repairSymbol_t repairSymbol;
+} mapStruct_t;
+
+/* CONVOLUTION */
+typedef struct fecConvolution {
+    __u32 encodingSymbolID;
+    __u16 repairKey;
+    __u8 ringBuffSize; // Number of packets for next coding in the ring buffer
+    struct sourceSymbol_t sourceRingBuffer[RLC_BUFFER_SIZE];
+    struct tlvRepair__convo_t repairTlv;
+} fecConvolution_t;
+
+typedef struct {
+    __u8 *muls;
+    struct repairSymbol_t *repairSymbol;
+} encode_rlc_t;
